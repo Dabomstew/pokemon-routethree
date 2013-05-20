3b62715500a1730f2ae83610477308bbc6184b30
Stewart Marshall <dbs.stew@gmail.com>
2013-05-20 22:57:52 +1200

    Working on an order patch.

--- src/BattleOptions.java
+++ src/BattleOptions.java
@@ -1,67 +1,77 @@
+import java.util.List;
 
 public class BattleOptions {
-    private int participants = 1;
-    private boolean printSRsOnLvl = false;
-    private boolean printSRsBoostOnLvl = false;
-    private StatModifier mod1;
-    private StatModifier mod2;
-    private int verbose = BattleOptions.NONE;
-    //verbose options
-    public static final int NONE = 0;
-    public static final int SOME = 1;
-    public static final int ALL = 2;
-    
-    public BattleOptions() {
-        setMod1(new StatModifier());
-        setMod2(new StatModifier());
-    }
+	private int participants = 1;
+	private boolean printSRsOnLvl = false;
+	private boolean printSRsBoostOnLvl = false;
+	private StatModifier mod1;
+	private StatModifier mod2;
+	private int verbose = BattleOptions.NONE;
+	// verbose options
+	public static final int NONE = 0;
+	public static final int SOME = 1;
+	public static final int ALL = 2;
+	private List<Integer> sendoutOrder = null;
 
-    public boolean isPrintSRsBoostOnLvl() {
-        return printSRsBoostOnLvl;
-    }
+	public BattleOptions() {
+		setMod1(new StatModifier());
+		setMod2(new StatModifier());
+	}
 
-    public void setPrintSRsBoostOnLvl(boolean printSRsBoostOnLvl) {
-        this.printSRsBoostOnLvl = printSRsBoostOnLvl;
-    }
+	public boolean isPrintSRsBoostOnLvl() {
+		return printSRsBoostOnLvl;
+	}
 
-    public int getParticipants() {
-        return participants;
-    }
+	public void setPrintSRsBoostOnLvl(boolean printSRsBoostOnLvl) {
+		this.printSRsBoostOnLvl = printSRsBoostOnLvl;
+	}
 
-    public void setParticipants(int participants) {
-        this.participants = participants;
-    }
+	public int getParticipants() {
+		return participants;
+	}
 
-    public boolean isPrintSRsOnLvl() {
-        return printSRsOnLvl;
-    }
+	public void setParticipants(int participants) {
+		this.participants = participants;
+	}
 
-    public void setPrintSRsOnLvl(boolean printSRsOnLvl) {
-        this.printSRsOnLvl = printSRsOnLvl;
-    }
+	public boolean isPrintSRsOnLvl() {
+		return printSRsOnLvl;
+	}
 
-    public StatModifier getMod1() {
-        return mod1;
-    }
+	public void setPrintSRsOnLvl(boolean printSRsOnLvl) {
+		this.printSRsOnLvl = printSRsOnLvl;
+	}
 
-    public void setMod1(StatModifier mod1) {
-        this.mod1 = mod1;
-    }
+	public StatModifier getMod1() {
+		return mod1;
+	}
 
-    public StatModifier getMod2() {
-        return mod2;
-    }
+	public void setMod1(StatModifier mod1) {
+		this.mod1 = mod1;
+	}
 
-    public void setMod2(StatModifier mod2) {
-        this.mod2 = mod2;
-    }
+	public StatModifier getMod2() {
+		return mod2;
+	}
 
-    public int getVerbose() {
-        return verbose;
-    }
+	public void setMod2(StatModifier mod2) {
+		this.mod2 = mod2;
+	}
 
-    public void setVerbose(int verbose) {
-        this.verbose = verbose;
-    }
-    
+	public int getVerbose() {
+		return verbose;
+	}
+
+	public void setVerbose(int verbose) {
+		this.verbose = verbose;
+	}
+
+	public void setTrainerSendoutOrder(List<Integer> order) {
+		this.sendoutOrder = order;
+	}
+
+	public List<Integer> getTrainerSendoutOrder() {
+		return this.sendoutOrder;
+	}
+
 }
